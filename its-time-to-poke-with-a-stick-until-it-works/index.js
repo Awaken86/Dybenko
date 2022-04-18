c4817de616d354dadcf016b91c39388d8c253ae4
awaken <matvey.zorka@gmail.com>
2022-04-18 23:13:26 +0300

    PostJs

---
+++ its-time-to-poke-with-a-stick-until-it-works/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+const PORT = 5000;
+const DB_URL = `mongodb+srv://user:user@cluster0.1okoj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json('Сервер робит')
+})
+async function startApp(){
+    try {
+        mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true})
+        app.listen(PORT, () => console.log('Server started on port:' + PORT))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+startApp()
