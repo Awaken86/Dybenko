975d7da9d27e0ca4dcae346f04c314a640ee0571
awaken <matvey.zorka@gmail.com>
2022-04-20 23:23:03 +0300

    20.04.22

--- its-time-to-poke-with-a-stick-until-it-works/index.js
+++ its-time-to-poke-with-a-stick-until-it-works/index.js
@@ -1,19 +1,18 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import Post from './Post.js';
+import router from './router.js';
 
-
-const PORT = 5000;
+const PORT = 5000
 const DB_URL = `mongodb+srv://user:user@cluster0.1okoj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
-const app = express();
+const app = express()
 
-app.get('/', (req, res) => {
-    res.status(200).json('Сервер робит')
-})
-async function startApp(){
+app.use(express.json())
+app.use('/api', router)
+
+async function startApp() {
     try {
-        mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true})
+        mongoose.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
         app.listen(PORT, () => console.log('Server started on port:' + PORT))
     } catch (e) {
         console.log(e)
