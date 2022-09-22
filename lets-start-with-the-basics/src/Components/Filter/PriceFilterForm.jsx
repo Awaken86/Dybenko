464d4cdf10e06f7b5d9efdddd39a0b41d1bbebc8
Matvey Awaken <matvey.zorka@gmail.com>
2022-09-22 22:58:40 +0300

    editmode

--- lets-start-with-the-basics/src/Components/Filter/PriceFilterForm.jsx
+++ lets-start-with-the-basics/src/Components/Filter/PriceFilterForm.jsx
@@ -1,9 +1,21 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik';
-import React from 'react';
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 import style from './PriceFilterForm.module.css'
 
 export const PriceFilterForm = React.memo((props) => {
+    let [editMode, setEditMode] = useState(false)
+    
+    const activeEditMode = () => {
+        setEditMode(true)
+    }
+    const deactiveEditMode = () => {
+        setEditMode(false)
+        //props.updateStatus(status);
+    }
+    // const onStatusChange = (e) => {
+    //     setStatus(e.currentTarget.value)
+    // }
     let PriceInfo = {
         maxPrice: props.maxPrice,
         minPrice: props.minPrice
@@ -33,20 +45,24 @@ export const PriceFilterForm = React.memo((props) => {
                         <Form>
                             <div>
                                 {!editMode &&
-                                    <div>
-                                        <span onDoubleClick={activeEditMode}>{props.status ? props.status : '"without status"'}</span>
+                                    <div onDoubleClick={activeEditMode}>
+                                        <span className={style.PriceColor}>{PriceInfo.maxPrice ? PriceInfo.maxPrice
+                                            : 'noPrice'}</span>
+                                        <br />
+                                        <span className={style.PriceColor}>{PriceInfo.minPrice ? PriceInfo.minPrice
+                                            : 'noPrice'}</span>
                                     </div>
                                 }
                                 {editMode &&
-                                    <div>
-                                        <input onChange={onStatusChange} autoFocus={true} value={status} onBlur={deactiveEditMode} />
-                                        <Field type="input" name="maxPrice" className={style.inputElem} />
+                                    <div onBlur={deactiveEditMode}>
+                                        <Field value={PriceInfo.maxPrice} type="input" name="maxPrice" className={style.inputElem} />
                                         <ErrorMessage name="maxPrice" component="div" />
+                                        <Field value={PriceInfo.minPrice} type="input" name="minPrice" className={style.inputElem} />
+                                        <ErrorMessage name="minPrice" component="div" />
                                     </div>
                                 }
                             </div>
-                            <Field type="input" name="minPrice" className={style.inputElem} />
-                            <ErrorMessage name="minPrice" component="div" />
+
                             <button type="submit" disabled={isSubmitting}>
                                 Submit
                             </button>
