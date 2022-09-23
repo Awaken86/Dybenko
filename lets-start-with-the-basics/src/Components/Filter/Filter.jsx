9905c29c49defc570efd1b251669bbb4c0bef3af
Matvey Awaken <matvey.zorka@gmail.com>
2022-09-24 00:00:30 +0300

    fixColorFilter

--- lets-start-with-the-basics/src/Components/Filter/Filter.jsx
+++ lets-start-with-the-basics/src/Components/Filter/Filter.jsx
@@ -6,19 +6,21 @@ import style from './Filter.module.css';
 import { PriceFilterForm } from './PriceFilterForm';
 
 const Filter = (props) => {
-    const [color, setColor] = useState('withoutFilter')
-    let colorArray = ['red', 'white', 'black']
+    const colorArray = ['red', 'white', 'black']
     const dispatch = useDispatch()
-    let setSelectedPriceHandler = (selectedPrice) => {
+    const setSelectedPriceHandler = (selectedPrice) => {
         dispatch(actions.setSelectedPrice(selectedPrice))
     }
-    let setFilterHandler = () => {
-        dispatch(actions.setColorFilter(color))
-    }
-    let filterCleaner = () => {
+    const filterCleaner = () => {
         dispatch(actions.setColorFilter('withoutFilter'))
     }
-
+    const SelectButton = (props) => {
+        return (
+            <button className={style.button} style={{ backgroundColor: props.color }} onClick={() => {
+                dispatch(actions.setColorFilter(props.color))
+            }}></button>
+        )
+    }
     return (
         <Nav className={style.NavContainer}>
             <Card >
@@ -27,8 +29,7 @@ const Filter = (props) => {
                     max:{props.maxPrice}
                     <br />
                     mix:{props.minPrice}
-                    <Nav>{colorArray.map((i) => <SelectButton key={i} setColor={setColor} color={i} />)}</Nav>
-                    <Button variant='dark' onClick={setFilterHandler}>Фильтр</Button>
+                    <Nav>{colorArray.map((i) => <SelectButton key={i} color={i} />)}</Nav>
                     <Button variant='dark' onClick={filterCleaner}>Очистить фильтр</Button>
                 </Card.Body>
             </Card>
@@ -38,8 +39,3 @@ const Filter = (props) => {
 
 export default Filter;
 
-export const SelectButton = (props) => {
-    return (
-        <button className={style.button} style={{ backgroundColor: props.color }} onClick={() => { props.setColor(props.color) }}></button>
-    )
-}
